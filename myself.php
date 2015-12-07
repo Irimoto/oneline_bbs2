7d9b33fa7ac41d9b5b34dd75dd6f8196569fa563
Hiroshi irimoto <toyota40906@gmail.com>
2015-12-07 10:21:32 +0800

    そいや

--- myself.php
+++ myself.php
@@ -8,16 +8,13 @@
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
       $nickname = mysqli_real_escape_string($db,$_POST['nickname']);
       $comment = mysqli_real_escape_string($db,$_POST['comment']);
-
       $sql = sprintf('INSERT INTO posts SET nickname="%s", comment="%s", created=NOW()',
             $nickname,
             $comment
       );
-
       $_SESSION["nickname"] = $nickname;
-
       mysqli_query($db,$sql);
-      header('Location: boot.php');
+      header('Location: myself.php');
     }
 ?>
 
@@ -28,32 +25,39 @@
   <title>セブ掲示版</title>
 
   <!-- CSS -->
-  <link rel="stylesheet" href="assets/css/bootstrap.css">
-  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
-  <link rel="stylesheet" href="assets/css/form.css">
-  <link rel="stylesheet" href="assets/css/timeline2.css">
-  <link rel="stylesheet" href="assets/css/main.css">
+  <link rel="stylesheet" href="assets2/css/bootstrap.css">
+  <link rel="stylesheet" href="assets2/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="assets2/css/form.css">
+  <link rel="stylesheet" href="assets2/css/timeline2.css">
+  <link rel="stylesheet" href="assets2/css/main.css">
 </head>
 <body>
 
   <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
           <div class="navbar-header page-scroll">
-              <a class="navbar-brand" href="#page-top"><span class="strong-title"><i class="fa fa-twitter-square"></i> Oneline bbs Twiiter</span></a>
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                  <span class="sr-only">Toggle navigation</span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+                  <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#page-top"><span class="strong-title"><i class="fa fa-twitter"></i> ONELINE_BBS TWITTER</span></a>
           </div>
+
           <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav navbar-right">
+              </ul>
           </div>
       </div>
   </nav>
 
   <div class="container">
     <div class="row">
-      <div class="col-md-12 content-margin-top">
-        <form action="boot.php" method="post">
+      <div class="col-md-3 content-margin-top">
+        <form action="myself.php" method="post">
           <div class="form-group">
             <div class="input-group">
-              <?php // echo '<input type="text" name="nickname" class="form-control" id="validate-text" placeholder="nickname" value="' . $_SESSION["nickname"] . '" required>'  ?>
-              
               <?php 
                   if (isset($_SESSION["nickname"])) {
                       echo sprintf('<input type="text" name="nickname" class="form-control"
@@ -82,61 +86,38 @@
         </form>
       </div>
 
-      <div class="col-md-12 content-margin-top">
-        <?php
-            // データの取得と表示
-            $sql = 'SELECT * FROM posts ORDER BY `created` DESC';
-            $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
-        ?>
+    <div class="col-md-9 content-margintop">
+      <?php
+       $sql = 'SELECT * FROM posts ORDER BY `created` DESC';
+       $posts = mysqli_query($db,$sql) or die(mysqli_error($db));
+      ?>
+      <ul class="timeline">
+      <?php while ($post = mysqli_fetch_assoc($posts)): ?>
 
-        <div class="timeline-centered">
-
-        <?php while ($post = mysqli_fetch_assoc($posts)): ?>
-
-        <article class="timeline-entry">
-
-            <div class="timeline-entry-inner">
-
-                <div class="timeline-icon bg-success">
-                    <i class="entypo-feather"></i>
-                    <i class="fa fa-cogs"></i>
-                </div>
-
-                <div class="timeline-label">
-                    <h2><a href="#"><?php echo $post['nickname'] ?></a> <span><?php echo $post['created'] ?></span></h2>
-                    <p><?php echo $post['comment'] ?></p>
-                </div>
+        <li class="timeline-inverted">
+          <div class="timeline-badge">
+            <i class="glyphicon glyphicon-ok-sign"></i></div>
+          
+          <div class="timeline-panel">
+            <div class="timeline-heading">
+              <h4 class="timeline-title"><?php echo $post['nickname']?></h4>
+              <p><small class="text-muted"><i class="glyphicon glyphicon-time"></i> <?php echo $post['created']?></small></p>
             </div>
-
-        </article>
+            <div class="timeline-body">
+              <p><?php echo $post['comment']?></p>
+            </div>
+          </div>
+          </li>
         <?php endwhile; ?>
-
-        <article class="timeline-entry begin">
-
-            <div class="timeline-entry-inner">
-
-                <div class="timeline-icon" style="-webkit-transform: rotate(-90deg); -moz-transform: rotate(-90deg);">
-                    <i class="entypo-flight"></i> +
-                </div>
-
-            </div>
-
-        </article>
-
-      </div>
-
+      </ul>
     </div>
-  </div>
-
-
-
-
-
-  
-  <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-  <!-- Include all compiled plugins (below), or include individual files as needed -->
-  <script src="assets/js/bootstrap.js"></script>
-  <script src="assets/js/form.js"></script>
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="assets/js/bootstrap.js"></script>
+<script src="assets/js/form.js"></script>
 </body>
 </html>
+
+
+
